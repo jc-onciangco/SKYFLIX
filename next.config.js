19d4c5095a4bc13c578ab757ec9295c8174a67c0
uwang20 <ohjuan205@gmail.com>
2022-04-02 12:31:30 +0800

    upgrading next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,  
+  images: {
+    domains: ['image.tmdb.org'],
+  },
 }
 
 module.exports = nextConfig
